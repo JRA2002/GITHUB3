271e110a7a4ed657c1d9bae98fc7b1c841099f37
Javier Rojas A. <105953543+JRA2002@users.noreply.github.com>
2025-05-23 17:31:41 +0200

    Add files via upload

---
+++ src/javierRojas/pedirNombre.java
@@ -0,0 +1,11 @@
+package javierRojas;
+import java.util.Scanner;
+public class pedirNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "javieRojas";
+		System.out.println("Mi nombre es  " + nombre);
+	}
+
+}
